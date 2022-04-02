c66d502939515864c574adb37be2e0ee7983a01e
Aleksi Backman <bddubbb@gmail.com>
2022-04-02 14:45:47 +0300

    Add source for minimal kernel elf
    Kernel expects to be loaded by a Stivale2 supporting bootloader.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hog
 Hog Operating System
+
+References used:
+* https://github.com/stivale/stivale2-barebones
---
+++ src/hog_main.c
@@ -0,0 +1,41 @@
+#include <stivale2.h>
+
+static uint8_t stack[8192];
+
+// TODO comments
+static struct stivale2_header_tag_terminal terminal_hdr_tag = {
+    .tag = {
+        .identifier = STIVALE2_HEADER_TAG_TERMINAL_ID,
+        .next = 0
+    },
+    .flags = 0
+};
+
+// TODO comments
+static struct stivale2_header_tag_framebuffer framebuffer_hdr_tag = {
+    .tag = {
+        .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
+        .next = (uint64_t)&terminal_hdr_tag
+    },
+    .framebuffer_width  = 0,
+    .framebuffer_height = 0,
+    .framebuffer_bpp    = 0
+};
+
+// TODO comments
+__attribute__((section(".stivale2hdr"), used))
+static struct stivale2_header stivale_hdr = {
+    .entry_point = 0,
+    .stack = (uintptr_t)stack + sizeof(stack),
+    .flags = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4),
+    .tags = (uintptr_t)&framebuffer_hdr_tag
+};
+
+// TODO comments
+void _start(struct stivale2_struct* stivale2_struct) {
+    (void) stivale2_struct;
+
+    for (;;) {
+        asm ("hlt");
+    }
+}
