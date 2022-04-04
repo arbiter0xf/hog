79bbada2c8c90a60a5f5e11161973cbe6e47e766
Aleksi Backman <bddubbb@gmail.com>
2022-04-04 20:47:35 +0300

    Extract functions from _start function

--- src/hog_main.c
+++ src/hog_main.c
@@ -6,14 +6,16 @@
 
 extern int is_cpuid_supported(void);
 
-void halt_execution(void)
+static void (*term_write)(const char* str, size_t len) = 0;
+
+static void halt_execution(void)
 {
     for (;;) {
         asm ("hlt");
     }
 }
 
-void (*get_term_write_function(struct stivale2_struct* stivale2_struct))(const char* str, size_t len)
+static void (*get_term_write_function(struct stivale2_struct* stivale2_struct))(const char* str, size_t len)
 {
     struct stivale2_struct_tag_terminal* tag_terminal = 0;
     void* terminal_write_ptr = 0;
@@ -28,23 +30,32 @@ void (*get_term_write_function(struct stivale2_struct* stivale2_struct))(const c
     return terminal_write_ptr;
 }
 
+static void detect_cpuid_support()
+{
+    uint32_t cpuid_supported = 0;
+    char cpuid_supported_hex_str[HEX_STR_SIZE_32] = {0};
+
+    term_write("Detecting CPUID support\n", 24);
+    cpuid_supported = is_cpuid_supported();
+    uint32_to_hex_str(cpuid_supported, cpuid_supported_hex_str);
+    term_write("CPUID supported: ", 17);
+    term_write(cpuid_supported_hex_str, 8);
+    term_write("\n", 1);
+}
+
+static void initialize_terminal_printing(struct stivale2_struct* stivale2_struct)
+{
+    term_write = get_term_write_function(stivale2_struct);
+}
+
 // TODO comments
 // https://github.com/stivale/stivale/blob/master/STIVALE2.md#kernel-entry-machine-state
 void _start(struct stivale2_struct* stivale2_struct)
 {
-    void (*term_write)(const char* str, size_t len) = 0;
-    uint32_t cpuid_supported = 0;
-    char cpuid_supported_hex_str[HEX_STR_SIZE_32] = {0};
-
-    cpuid_supported = is_cpuid_supported();
-    uint32_to_hex_str(cpuid_supported, cpuid_supported_hex_str);
-
-    term_write = get_term_write_function(stivale2_struct);
-
+    initialize_terminal_printing(stivale2_struct);
     term_write("Terminal test print\n", 20);
-    term_write("CPUID supported: ", 17);
-    term_write(cpuid_supported_hex_str, 8);
-    term_write("\n", 1);
+
+    detect_cpuid_support();
 
     halt_execution();
 }
